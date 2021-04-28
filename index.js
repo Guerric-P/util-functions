196e45bd9a0a2b975a59e84a3d9d476ad1fdb8cb
Guerric Phalippou <guerric.phalippou@gadz.org>
2021-04-28 17:59:30 +0200

    prettier

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function pair(items, number = 2) {
-  return items.reduce((acc, cur, i) => i % number ? Object.assign([...acc], {
-      [acc.length - 1]: [...acc[acc.length - 1], cur]
-    }) : [...acc, [cur]], []);
+  return items.reduce(
+    (acc, cur, i) =>
+      i % number
+        ? Object.assign([...acc], {
+            [acc.length - 1]: [...acc[acc.length - 1], cur],
+          })
+        : [...acc, [cur]],
+    []
+  );
 }
