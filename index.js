98748561e443a89457f72391a27ddab04d16a579
Guerric Phalippou <guerric.phalippou@gadz.org>
2021-04-28 17:53:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function pair(items, number = 2) {
+  return items.reduce((acc, cur, i) => i % number ? Object.assign([...acc], {
+      [acc.length - 1]: [...acc[acc.length - 1], cur]
+    }) : [...acc, [cur]], []);
+}
