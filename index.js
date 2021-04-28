d4050516ad58e8d66518dbc42bbc16ad9e9a2859
Guerric Phalippou <guerric.phalippou@gadz.org>
2021-04-28 18:08:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ function pair(arr, number = 2) {
 function groupNeighbors(arr) {
   return arr.reduce(
     (acc, cur, i, { [i - 1]: last }) =>
-      (cur === last ? acc[acc.length - 1].push(cur) : acc.push([cur])) && acc,
+      cur === last
+        ? Object.assign([...acc], {
+            [acc.length - 1]: [...acc[acc.length - 1], cur],
+          })
+        : [...acc, [cur]],
     []
   );
 }
